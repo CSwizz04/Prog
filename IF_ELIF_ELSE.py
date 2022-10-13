edbc9632d23e3ffa72364e6b09456f35ab99b4c1
CSwizz04 <czaipangan.08@gmail.com>
2022-10-14 02:52:10 +0900

    Prog

---
+++ IF_ELIF_ELSE.py
@@ -0,0 +1,24 @@
+"""
+If, Elif, Else 
+
+Program: IF_ELIF_ELSE.py
+Author: Chryztian Pangan
+Last Date Modified: 10/12/22
+the purpose of this program is that it is to 
+demonstrate the excercise of If, Elif, and Else phyton script. 
+
+"""
+age = int(input("how old are you")) 
+if age > 30: 
+    print ("they are getting old")
+
+if age %2 ==0:
+    print("Your age is an even number")
+else: 
+    print("Your age is an odd number")
+
+name = input("enter namne:")
+first_letter = ord(name[0])
+print("the first letter in ASCII is ", first_letter)
+
+fav_food 
